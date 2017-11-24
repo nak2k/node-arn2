7b97d1d58a993bc4eb7aeb6ee18475e9e34687b6
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2017-11-24 14:35:42 +0900

    Refactor

--- test/test.js
+++ test/test.js
@@ -2,73 +2,119 @@ const test = require('tape');
 const { parseArn } = require('..');
 
 test('test parseArn', t => {
-  t.plan(7);
+  t.plan(2);
 
   const arn = 'arn:aws:apigateway:us-west-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-west-2:123456789012:function:Test/invocations';
 
   parseArn(arn, (err, obj) => {
     t.error(err);
 
-    t.equal(obj.service, 'apigateway');
-    t.equal(obj.region, 'us-west-2');
+    t.deepEqual(obj, {
+      arn,
+      scheme: 'arn',
+      partition: 'aws',
+      service: 'apigateway',
+      region: 'us-west-2',
+      account: 'lambda',
+      resource: 'path/2015-03-31/functions/arn:aws:lambda:us-west-2:123456789012:function:Test/invocations',
+      resourceType: 'path/2015-03-31/functions/arn',
+      colonSeparatedResource: ['path/2015-03-31/functions/arn', 'aws', 'lambda', 'us-west-2', '123456789012', 'function', 'Test/invocations'],
 
-    const { apigateway } = obj;
-    t.equal(typeof(apigateway), 'object');
+      apigateway: {
+        lambdaIntegration: {
+          arn: 'arn:aws:lambda:us-west-2:123456789012:function:Test',
+          scheme: 'arn',
+          partition: 'aws',
+          service: 'lambda',
+          region: 'us-west-2',
+          account: '123456789012',
+          resource: 'function:Test',
+          resourceType: 'function',
+          colonSeparatedResource: ['function', 'Test'],
 
-    const { lambdaIntegration } = apigateway;
-    t.equal(typeof(lambdaIntegration), 'object');
-    t.equal(typeof(lambdaIntegration.lambda), 'object');
-    t.equal(lambdaIntegration.lambda.functionName, 'Test');
+          lambda: {
+            functionName: 'Test',
+            version: undefined,
+          },
+        }
+      },
+    });
   });
 });
 
 test('test parseArn', t => {
-  t.plan(7);
+  t.plan(2);
 
   const arn = 'arn:aws:apigateway:us-west-2:s3:path/test-bucket/{folder}/{item}';
 
   parseArn(arn, (err, obj) => {
     t.error(err);
 
-    t.equal(obj.service, 'apigateway');
-    t.equal(obj.region, 'us-west-2');
+    t.deepEqual(obj, {
+      arn,
+      scheme: 'arn',
+      partition: 'aws',
+      service: 'apigateway',
+      region: 'us-west-2',
+      account: 's3',
+      resource: 'path/test-bucket/{folder}/{item}',
+      resourceType: 'path/test-bucket/{folder}/{item}',
+      colonSeparatedResource: ['path/test-bucket/{folder}/{item}'],
 
-    const { apigateway } = obj;
-    t.equal(typeof(apigateway), 'object');
-
-    const { s3Integration } = apigateway;
-    t.equal(typeof(s3Integration), 'object');
-    t.equal(s3Integration.bucket, 'test-bucket');
-    t.equal(s3Integration.key, '/{folder}/{item}');
+      apigateway: {
+        s3Integration: {
+          bucket: 'test-bucket',
+          key: '/{folder}/{item}',
+        }
+      },
+    });
   });
 });
 
 test('test parseArn for SNS', t => {
-  t.plan(4);
+  t.plan(2);
 
   const arn = 'arn:aws:sns:us-west-2:123456789012:test-topic';
 
   parseArn(arn, (err, obj) => {
     t.error(err);
 
-    t.equal(obj.service, 'sns');
+    t.deepEqual(obj, {
+      arn,
+      scheme: 'arn',
+      partition: 'aws',
+      service: 'sns',
+      region: 'us-west-2',
+      account: '123456789012',
+      resource: 'test-topic',
+      resourceType: 'test-topic',
+      colonSeparatedResource: ['test-topic'],
 
-    const { sns } = obj;
-    t.equal(typeof(sns), 'object');
-    t.equal(sns.topicName, 'test-topic');
+      sns: {
+        topicName: 'test-topic',
+      },
+    });
   });
 });
 
 test('test parseArn for DynamoDB', t => {
-  t.plan(4);
+  t.plan(2);
 
   const arn = 'arn:aws:dynamodb:us-west-2:123456789012:table/TEST';
 
   parseArn(arn, (err, obj) => {
     t.error(err);
 
-    t.equal(obj.service, 'dynamodb');
-    t.equal(obj.resourceType, 'table');
-    t.equal(obj.resource, 'TEST');
+    t.deepEqual(obj, {
+      arn,
+      scheme: 'arn',
+      partition: 'aws',
+      service: 'dynamodb',
+      region: 'us-west-2',
+      account: '123456789012',
+      resource: 'TEST',
+      resourceType: 'table',
+      colonSeparatedResource: ['table/TEST'],
+    });
   });
 });
