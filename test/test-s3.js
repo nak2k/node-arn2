21e6baaf2e727591b41533faf8e79e3f625afd36
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-07-02 23:04:17 +0900

    Add parseS3Arn

---
+++ test/test-s3.js
@@ -0,0 +1,56 @@
+const test = require('tape');
+const { parseArn } = require('..');
+
+test('test parseArn for S3', t => {
+  t.plan(2);
+
+  const arn = 'arn:aws:s3:::my-bucket';
+
+  parseArn(arn, (err, obj) => {
+    t.error(err);
+
+    t.deepEqual(obj, {
+      arn,
+      scheme: 'arn',
+      partition: 'aws',
+      service: 's3',
+      region: '',
+      account: '',
+      resource: 'my-bucket',
+      resourceType: 'my-bucket',
+      colonSeparatedResource: ['my-bucket'],
+
+      s3: {
+        bucket: 'my-bucket',
+        key: undefined,
+      },
+    });
+  });
+});
+
+test('test parseArn for S3', t => {
+  t.plan(2);
+
+  const arn = 'arn:aws:s3:::my-bucket/folder1/folder2/test.png';
+
+  parseArn(arn, (err, obj) => {
+    t.error(err);
+
+    t.deepEqual(obj, {
+      arn,
+      scheme: 'arn',
+      partition: 'aws',
+      service: 's3',
+      region: '',
+      account: '',
+      resource: 'my-bucket/folder1/folder2/test.png',
+      resourceType: 'my-bucket/folder1/folder2/test.png',
+      colonSeparatedResource: ['my-bucket/folder1/folder2/test.png'],
+
+      s3: {
+        bucket: 'my-bucket',
+        key: 'folder1/folder2/test.png',
+      },
+    });
+  });
+});
