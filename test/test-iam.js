6811dac267c9e500ed361843b571f66a02344c74
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-09-05 08:07:06 +0900

    Test

---
+++ test/test-iam.js
@@ -0,0 +1,20 @@
+const test = require('tape');
+const { parseAndDeepEqual } = require('./helper');
+
+test('test parseArn for IAM', t => {
+  t.plan(2);
+
+  parseAndDeepEqual(t,
+    'arn:aws:iam::123456789012:root',
+    {
+      scheme: 'arn',
+      partition: 'aws',
+      service: 'iam',
+      region: '',
+      account: '123456789012',
+      resource: undefined,
+      resourceType: 'root',
+      colonSeparatedResource: ['root'],
+    }
+  );
+});
