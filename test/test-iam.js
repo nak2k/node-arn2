12dee90e4fcf1724dc4f1c437007e3e21904c9d6
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-09-16 15:33:22 +0900

    Test

--- test/test-iam.js
+++ test/test-iam.js
@@ -18,3 +18,21 @@ test('test parseArn for IAM', t => {
     }
   );
 });
+
+test('test parseArn for IAM', t => {
+  t.plan(2);
+
+  parseAndDeepEqual(t,
+    'arn:aws:iam::123456789012:role/test-role',
+    {
+      scheme: 'arn',
+      partition: 'aws',
+      service: 'iam',
+      region: '',
+      account: '123456789012',
+      resource: 'test-role',
+      resourceType: 'role',
+      colonSeparatedResource: ['role/test-role'],
+    }
+  );
+});
